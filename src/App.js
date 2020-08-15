e62cfe8a0d87e88c35f2102b7ded3af1f9fd4474
Steele Schauer <steele@ivysys.com>
2020-08-14 23:24:46 -0400

    setting up auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import './App.scss';
 import InnerApp from './innerApp';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 
 const store = configureStore();
 
@@ -11,10 +13,16 @@ class App extends React.Component {
   render() {
     return(
         <Provider store={store}>
-            <InnerApp/>
+          <div className="App">
+            <header>
+              <h1>We now have Auth!</h1>
+            </header>
+            <AmplifySignOut />
+          </div>
+          <InnerApp/>
         </Provider>
     )
   };
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 ReactDOM.render((<BrowserRouter> <App /></BrowserRouter>), document.getElementById('root'));
 
